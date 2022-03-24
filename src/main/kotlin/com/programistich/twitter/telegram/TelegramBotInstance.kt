1b43e810c25dba197519d95437586e828fc9b159
Dzhos Oleksii <oleksii.dzhos@ukma.edu.ua>
2022-03-24 17:36:59 +0200

    logger update

--- src/main/kotlin/com/programistich/twitter/telegram/TelegramBotInstance.kt
+++ src/main/kotlin/com/programistich/twitter/telegram/TelegramBotInstance.kt
@@ -1,5 +1,6 @@
 package com.programistich.twitter.telegram
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
@@ -11,6 +12,8 @@ class TelegramBotInstance(
     @Lazy private val telegramBotRouting: TelegramBotRouting
 ) : TelegramLongPollingBot() {
 
+    private val logger = LoggerFactory.getLogger(this::class.java.name)
+
     override fun getBotToken(): String {
         return telegramBotConfiguration.token
     }
@@ -21,6 +24,7 @@ class TelegramBotInstance(
 
     override fun onUpdateReceived(update: Update?) {
         if(update == null) throw NullPointerException("Telegram update null")
+        logger.info("$update")
         telegramBotRouting.entryPointUpdate(update)
     }
 }

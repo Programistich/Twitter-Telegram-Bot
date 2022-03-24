93fa3b21c7b2b4f93f6eb61798a541f15dc76fb6
Dzhos Oleksii <oleksii.dzhos@ukma.edu.ua>
2022-03-24 19:25:55 +0200

    fix cronjob delay

--- src/main/kotlin/com/programistich/twitter/cronjob/stocks/StockCronJob.kt
+++ src/main/kotlin/com/programistich/twitter/cronjob/stocks/StockCronJob.kt
@@ -1,8 +1,17 @@
 package com.programistich.twitter.cronjob.stocks
 
+import com.programistich.twitter.service.stocks.StocksService
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
+
 @Service
-interface StockCronJob {
-    fun getTeslaStocks()
+class StockCronJob(
+    private val stockService: StocksService,
+)  {
+
+    @Scheduled(cron = "0 0 22 * * *")
+    fun getTeslaStocks() {
+        stockService.sendStock("-1001488807577", null, stockService.getStock("TSLA"))
+    }
 }
--- src/main/kotlin/com/programistich/twitter/cronjob/twitter/TwitterCronJob.kt
+++ src/main/kotlin/com/programistich/twitter/cronjob/twitter/TwitterCronJob.kt
@@ -1,5 +1,93 @@
 package com.programistich.twitter.cronjob.twitter
 
-interface TwitterCronJob {
-    fun updateTwitterForAllUsernames()
+import com.programistich.twitter.common.TypeCommand
+import com.programistich.twitter.entity.TwitterUser
+import com.programistich.twitter.service.db.DefaultDatabaseTelegramChatService
+import com.programistich.twitter.service.db.DefaultDatabaseTwitterUserService
+import com.programistich.twitter.service.telegram.DefaultTelegramExecutorService
+import com.programistich.twitter.service.twitter.TwitterClientService
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import twitter4j.Tweet
+import java.util.concurrent.TimeUnit
+
+@Service
+@Transactional
+class TwitterCronJob(
+    private val defaultDatabaseTwitterUserService: DefaultDatabaseTwitterUserService,
+    private val databaseTelegramChatService: DefaultDatabaseTelegramChatService,
+    private val twitterClientService: TwitterClientService,
+    private val telegramExecutorService: DefaultTelegramExecutorService,
+)  {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Scheduled(fixedDelay = 2 * 60 * 1000)
+    fun updateTwitter() {
+        updateTwitterForAllUsernames()
+    }
+
+    private fun updateTwitterForAllUsernames() {
+        logger.info("Start update twitter accounts")
+        val usernames = defaultDatabaseTwitterUserService.getAllUsername()
+        usernames.forEach {
+            val existUsername = defaultDatabaseTwitterUserService.existUser(it)
+            if (!existUsername) {
+                logger.info("Username = $it not found in db")
+                return
+            }
+            val tweetInDB = defaultDatabaseTwitterUserService.getTwitterUserByUsername(it)
+            updateLikeForUsername(it, tweetInDB)
+            TimeUnit.SECONDS.sleep(10)
+            updateTweetForUsername(it, tweetInDB)
+            TimeUnit.SECONDS.sleep(10)
+        }
+        logger.info("End update twitter accounts")
+    }
+
+    private fun updateTweetForUsername(username: String, tweetInDB: TwitterUser?) {
+        logger.info("Update twitter account $username")
+        val tweetInTwitter: Tweet = twitterClientService.lastTweetByUsername(username)
+        if (tweetInDB == null || tweetInTwitter.id != tweetInDB.lastTweetId) {
+            logger.info("New tweet from $username id = $tweetInTwitter.id")
+            val twitterUser = TwitterUser(
+                username = username,
+                lastLikeId = tweetInDB!!.lastLikeId,
+                lastTweetId = tweetInTwitter.id,
+                chats = tweetInDB.chats
+            )
+            defaultDatabaseTwitterUserService.updateTwitterUser(twitterUser)
+            logger.info("Update username = $username")
+            val chats = databaseTelegramChatService.getChatsByUsername(username)
+            chats.map {
+                logger.info("Send tweet to $it")
+                telegramExecutorService.sendTweetEntryPoint(tweetInTwitter.id, it)
+            }
+        }
+    }
+
+
+    private fun updateLikeForUsername(username: String, tweetInDB: TwitterUser?) {
+        val tweetInTwitter: Tweet = twitterClientService.lastLikeByUsername(username)
+        if (tweetInDB == null || tweetInTwitter.id != tweetInDB.lastLikeId) {
+            logger.info("New tweet from $username id = $tweetInTwitter.id")
+            val twitterUser = TwitterUser(
+                username = username,
+                lastLikeId = tweetInTwitter.id,
+                lastTweetId = tweetInDB!!.lastTweetId,
+                chats = tweetInDB.chats
+            )
+            defaultDatabaseTwitterUserService.updateTwitterUser(twitterUser)
+            logger.info("Update username = $username")
+            val parsedTweet = twitterClientService.parseTweet(tweetInTwitter.id)
+            val chats = databaseTelegramChatService.getChatsByUsername(username)
+            chats.map {
+                logger.info("Send tweet to $it")
+                val typeTweet = TypeCommand.Like(username, tweetInTwitter.id)
+                telegramExecutorService.sendTweet(it, parsedTweet, typeTweet)
+            }
+        }
+    }
 }
